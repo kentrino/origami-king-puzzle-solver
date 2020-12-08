08a6aa160506eb62920e39fd2079225aed9fc205
kentrino <kentrino@h-kento.jp>
2020-12-08 16:05:59 +0900

    (solve_multiprocess) mv functions

--- src/main.py
+++ src/main.py
@@ -1,42 +1,4 @@
-import asyncio
-from asyncio import AbstractEventLoop
-from concurrent.futures.process import ProcessPoolExecutor
-from typing import List
-
-import numpy as np
-
-from message_queue import MessageQueue
-from printer import Printer
-from run import RunContext, run, all_patterns
-
-
-async def async_run(
-        loop: AbstractEventLoop,
-        executor: ProcessPoolExecutor,
-        context: RunContext):
-    return await loop.run_in_executor(
-        executor,
-        run,
-        context)
-
-
-async def async_solve(loop: AbstractEventLoop, field_0: np.ndarray, queue: MessageQueue, n_process: int):
-    t = all_patterns // n_process
-    ranges = [range(i * t + 1, (i + 1) * t) for i in range(0, n_process)]
-    tasks = []
-    with ProcessPoolExecutor(max_workers=n_process) as executor:
-        for i, _range in enumerate(ranges):
-            tasks.append(asyncio.create_task(async_run(
-                loop,
-                executor,
-                RunContext(
-                    task_id=i,
-                    queue=queue,
-                    field_0=field_0,
-                    range=_range,
-                ))))
-        return await asyncio.gather(*tasks)
-
+from solve_multiprocess import solve_multiprocess
 
 _initial_field = [
     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,15 +6,6 @@ _initial_field = [
     [0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1],
     [1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0],
 ]
-
-
-def solve_multiprocess(initial_field: List[List[int]], n_process, debug_print: bool):
-    with Printer(lines=n_process, debug_print=debug_print) as p:
-        _loop = asyncio.get_event_loop()
-        results = _loop.run_until_complete(
-            async_solve(_loop, np.array(initial_field), queue=p.queue, n_process=n_process))
-    print(list(filter(lambda r: r is not None, results))[0])
-
 
 if __name__ == '__main__':
     solve_multiprocess(_initial_field, n_process=6, debug_print=True)
---
+++ src/solve_multiprocess.py
@@ -0,0 +1,46 @@
+import asyncio
+from asyncio import AbstractEventLoop
+from concurrent.futures.process import ProcessPoolExecutor
+from typing import List
+
+import numpy as np
+
+from message_queue import MessageQueue
+from printer import Printer
+from run import RunContext, run, all_patterns
+
+
+async def _async_run(
+        loop: AbstractEventLoop,
+        executor: ProcessPoolExecutor,
+        context: RunContext):
+    return await loop.run_in_executor(
+        executor,
+        run,
+        context)
+
+
+async def _async_solve(loop: AbstractEventLoop, field_0: np.ndarray, queue: MessageQueue, n_process: int):
+    t = all_patterns // n_process
+    ranges = [range(i * t + 1, (i + 1) * t) for i in range(0, n_process)]
+    tasks = []
+    with ProcessPoolExecutor(max_workers=n_process) as executor:
+        for i, _range in enumerate(ranges):
+            tasks.append(asyncio.create_task(_async_run(
+                loop,
+                executor,
+                RunContext(
+                    task_id=i,
+                    queue=queue,
+                    field_0=field_0,
+                    range=_range,
+                ))))
+        return await asyncio.gather(*tasks)
+
+
+def solve_multiprocess(initial_field: List[List[int]], n_process, debug_print: bool):
+    with Printer(lines=n_process, debug_print=debug_print) as p:
+        _loop = asyncio.get_event_loop()
+        results = _loop.run_until_complete(
+            _async_solve(_loop, np.array(initial_field), queue=p.queue, n_process=n_process))
+    print(list(filter(lambda r: r is not None, results))[0])
